ae5841fdab8c3e1910677d0ff49bf7f0d36e1232
elmiller <miller.1771@wright.edu>
2026-01-13 13:37:09 -0500

    modified script so it updates the server when images are added or removed from the directory after a refresh

--- site.py
+++ site.py
@@ -1,23 +1,25 @@
-from flask import Flask, send_from_directory, render_template
+from flask import Flask, send_from_directory, render_template, abort
 import os
 
 app = Flask(__name__)
 
 #IMAGE_FOLDER = "photo_site/photos"
-IMAGE_FOLDER = "static"
+IMAGE_FOLDER = os.path.join(os.getcwd(), "runtime-images")
 
 @app.route("/")
 def index():
-    files = os.path.join(app.static_folder, "images")
-    print("Image directory:", files)
-    print("Files found:", os.listdir(files))
-    image_files = [f for f in os.listdir(files) if f.lower().endswith((".png", ".jpg", ".jpeg", ".gif"))]
+    images = [f for f in os.listdir(IMAGE_FOLDER) if f.lower().endswith((".png", ".jpg", ".jpeg", ".gif"))]
 
-    return render_template("site-structure.html", images = image_files)
+    return render_template("site-structure.html", images = images)
 
+# abort is used here in case the path to the file name does not exist
 @app.route("/image/<filename>")
-def image(filename):
+def get_images(filename):
+    if not os.path.exists(os.path.join(IMAGE_FOLDER, filename)):
+        abort(404)
+    # if it does exist then return the file from that directory
     return send_from_directory(IMAGE_FOLDER, filename)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
